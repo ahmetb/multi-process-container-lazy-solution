44832935c09c73b9733b3cf1666f2f136947e6f0
Bogdan <madbbb@icloud.com>
2021-06-08 12:47:56 +0300

    aaa

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello_world():
     return 'Hello from the server, the time is {}'.format(datetime.now())
 
 if __name__ == "__main__":
-    app.run(debug=True,host='localhost',port=8081)
+    app.run(debug=True,host='0.0.0.0',port=8081)
