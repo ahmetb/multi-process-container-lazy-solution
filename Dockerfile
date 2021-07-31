26c7d3662306f687dadaf3d8ca22d9bb09ec30d4
Bogdan <madbbb@icloud.com>
2021-07-31 21:32:27 +0300

    feature: wait for services to start

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM python:3-alpine
 
-RUN apk add --no-cache \
-	nginx \
-	bash
+RUN apk add --no-cache nginx bash curl
 RUN pip install Flask
 ENV TINI_VERSION v0.18.0
 ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini-static /tini
--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-nginx -c "$PWD/nginx.conf" &
 flask run --port 8081 &
+echo "[***] Waiting for flusk to start"
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8081/)" != "200" ]]; do sleep 5; done
+echo "[***] Starting NGINX"
+nginx -c "$PWD/nginx.conf" &
+echo "[***] Acceping connections"
 
 wait -n
